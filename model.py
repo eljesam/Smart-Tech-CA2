d3a644039a16a5703e552a1119f1776ba1a1d2c6
eljesam <124920260+eljesam@users.noreply.github.com>
2026-01-10 16:08:26 +0000

    Creating model

---
+++ model.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (Conv2D, Dense, Flatten, Lambda, Dropout)
+from tensorflow.keras.optimizers import Adam
+
+def nvidia_model():
+    model = Sequential()
+
+    # Normalization layer
+    model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(66, 200, 3)))
+    
+    # Convolutional layers
+    model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    
+    # Fully connected layers
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
+    
+    optimizer = Adam(lr=1e-4)
+    model.compile(loss='mse', optimizer=optimizer)
+    
+    return model
