424ffd97803d2218c43fb720b5f4b627b79af586
eljes <d00251881@student.dkit.ie>
2026-08-31 19:13:26 -0400

    completed data augmentation

--- src/augmentation.py
+++ src/augmentation.py
@@ -39,3 +39,102 @@ def random_flip(image, steering):
         steering = -steering
 
     return image, steering
+
+#Brightness Augmentation
+def random_brightness(image):
+    
+
+    hsv = cv2.cvtColor(
+        image,
+        cv2.COLOR_RGB2HSV
+    )
+
+    brightness_factor = np.random.uniform(
+        0.6,
+        1.4
+    )
+
+    hsv = hsv.astype(np.float32)
+
+    hsv[:, :, 2] *= brightness_factor
+
+    hsv[:, :, 2] = np.clip(
+        hsv[:, :, 2],
+        0,
+        255
+    )
+
+    hsv = hsv.astype(np.uint8)
+
+    image = cv2.cvtColor(
+        hsv,
+        cv2.COLOR_HSV2RGB
+    )
+
+    return image
+
+#Generating augmented training sample
+def augment_sample(row):
+  
+
+    image, steering = select_camera(row)
+
+    image, steering = random_flip(
+        image,
+        steering
+    )
+
+    image = random_brightness(
+        image
+    )
+
+    return image, steering
+
+if __name__ == "__main__":
+
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from pathlib import Path
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    CSV_PATH = (
+        BASE_DIR
+        / "data"
+        / "raw"
+        / "driving_log_balanced.csv"
+    )
+
+    df = pd.read_csv(
+        CSV_PATH
+    )
+
+    row = df.iloc[100]
+
+    image, steering = augment_sample(
+        row
+    )
+
+    print(
+        "Original steering:",
+        row["steering"]
+    )
+
+    print(
+        "Augmented steering:",
+        steering
+    )
+
+    print(
+        "Image shape:",
+        image.shape
+    )
+
+    plt.imshow(image)
+
+    plt.title(
+        f"Augmented Image - Steering: {steering:.3f}"
+    )
+
+    plt.axis("off")
+    plt.show()
